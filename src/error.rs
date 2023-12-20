6bb613bae8850f7e43a8e733ac8ce9cd2c8f2815
Vidhan Bhatt <me@vidhan.io>
2023-12-20 13:40:15 -0500

    chore: fix clippy

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,5 @@
 use thiserror::Error;
 
-#[derive(Debug, Error)]
+#[derive(Debug, Clone, Copy, Error)]
+#[allow(clippy::module_name_repetitions)]
 pub enum {{ project-name | upper_camel_case }}Error {}
