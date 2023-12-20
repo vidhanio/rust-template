d7385d3deb645b8f292c647f91242f3b803758ca
Vidhan Bhatt <me@vidhan.io>
2023-12-20 13:45:41 -0500

    ci: fix clippy

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,13 @@ mod error;
 pub use self::error::{{ project-name | upper_camel_case }}Error;
 
 pub type {{ project-name | upper_camel_case }}Result<T> = std::result::Result<T, {{ project-name | upper_camel_case }}Error>;
-{% if crate_type == "bin" -%}
+
+/// Run the application.
+///
+/// # Errors
+///
+/// Returns an error if the application fails to run.
+{%- if crate_type == "bin" -%}
 {%- if async %}
 pub async fn run() -> {{ project-name | upper_camel_case }}Result<()> {
     std::future::ready(Ok(())).await
